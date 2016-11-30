deb4408711fec43fcd6f8da13405900e15bbed8a
jiaohy <jiaohy1010@gmail.com>
2016-11-30 16:33:12 -0600

    add all templates

--- pdollar.js
+++ pdollar.js
@@ -87,7 +87,7 @@ function Result(name, score) // constructor
 //
 // PDollarRecognizer class constants
 //
-var NumPointClouds = 16;
+var NumPointClouds = 7;
 var NumPoints = 32;
 var Origin = new Point(0,0,0);
 //
@@ -120,55 +120,41 @@ function PDollarRecognizer() // constructor
 		new Point(566,423,3),new Point(644,423,3),
 		new Point(644,393,4),new Point(644,423,4)
 	));
-	this.PointClouds[4] = new PointCloud("X", new Array(
-		new Point(30,146,1),new Point(106,222,1),
-		new Point(30,225,2),new Point(106,146,2)
+	this.PointClouds[4] = new PointCloud("switch", new Array(
+		new Point(356,469,1),new Point(358,449,1),new Point(361,434,1),new Point(368,413,1),new Point(377,400,1),new Point(389,385,1),
+		new Point(405,373,1),new Point(419,367,1),new Point(438,361,1),new Point(456,359,1),new Point(466,360,1),new Point(483,363,1),
+		new Point(496,367,1),new Point(512,375,1),new Point(527,387,1),new Point(537,400,1),new Point(547,416,1),new Point(554,434,1),
+		new Point(556,452,1),new Point(556,473,1),new Point(550,494,1),new Point(543,509,1),new Point(531,525,1),new Point(518,538,1),
+		new Point(509,543,1),new Point(497,550,1),new Point(478,556,1),new Point(457,558,1),new Point(435,556,1),new Point(419,551,1),
+		new Point(403,542,1),new Point(392,534,1),new Point(382,524,1),new Point(372,510,1),new Point(363,495,1),new Point(359,479,1),
+		new Point(358,470,1),
+		new Point(528,388,2),new Point(1017,27,2)
 	));
-	this.PointClouds[5] = new PointCloud("H", new Array(
-		new Point(188,137,1),new Point(188,225,1),
-		new Point(188,180,2),new Point(241,180,2),
-		new Point(241,137,3),new Point(241,225,3)
+	this.PointClouds[5] = new PointCloud("lamp", new Array(
+		new Point(356,469,1),new Point(358,449,1),new Point(361,434,1),new Point(368,413,1),new Point(377,400,1),new Point(389,385,1),
+		new Point(405,373,1),new Point(419,367,1),new Point(438,361,1),new Point(456,359,1),new Point(466,360,1),new Point(483,363,1),
+		new Point(496,367,1),new Point(512,375,1),new Point(527,387,1),new Point(537,400,1),new Point(547,416,1),new Point(554,434,1),
+		new Point(556,452,1),new Point(556,473,1),new Point(550,494,1),new Point(543,509,1),new Point(531,525,1),new Point(518,538,1),
+		new Point(509,543,1),new Point(497,550,1),new Point(478,556,1),new Point(457,558,1),new Point(435,556,1),new Point(419,551,1),
+		new Point(403,542,1),new Point(392,534,1),new Point(382,524,1),new Point(372,510,1),new Point(363,495,1),new Point(359,479,1),
+		new Point(358,470,1),
+		new Point(415,412,2),new Point(497,513,2),
+		new Point(415,513,3),new Point(497,410,3)
 	));
-	this.PointClouds[6] = new PointCloud("I", new Array(
-		new Point(371,149,1),new Point(371,221,1),
-		new Point(341,149,2),new Point(401,149,2),
-		new Point(341,221,3),new Point(401,221,3)
+	this.PointClouds[6] = new PointCloud("AC", new Array(
+		new Point(356,469,1),new Point(358,449,1),new Point(361,434,1),new Point(368,413,1),new Point(377,400,1),new Point(389,385,1),
+		new Point(405,373,1),new Point(419,367,1),new Point(438,361,1),new Point(456,359,1),new Point(466,360,1),new Point(483,363,1),
+		new Point(496,367,1),new Point(512,375,1),new Point(527,387,1),new Point(537,400,1),new Point(547,416,1),new Point(554,434,1),
+		new Point(556,452,1),new Point(556,473,1),new Point(550,494,1),new Point(543,509,1),new Point(531,525,1),new Point(518,538,1),
+		new Point(509,543,1),new Point(497,550,1),new Point(478,556,1),new Point(457,558,1),new Point(435,556,1),new Point(419,551,1),
+		new Point(403,542,1),new Point(392,534,1),new Point(382,524,1),new Point(372,510,1),new Point(363,495,1),new Point(359,479,1),
+		new Point(358,470,1),
+		new Point(372,459,2),new Point(379,475,2),new Point(387,488,2),new Point(396,494,2),new Point(406,500,2),new Point(416,501,2),
+		new Point(426,499,2),new Point(435,495,2),new Point(444,484,2),new Point(449,478,2),new Point(454,467,2),new Point(459,457,2),
+		new Point(464,443,2),new Point(470,434,2),new Point(475,427,2),new Point(482,422,2),new Point(490,417,2),new Point(501,416,2),
+		new Point(517,421,2),new Point(529,433,2),new Point(536,441,2),new Point(542,455,2)
 	));
-	this.PointClouds[7] = new PointCloud("exclamation", new Array(
-		new Point(526,142,1),new Point(526,204,1),
-		new Point(526,221,2)
-	));
-	this.PointClouds[8] = new PointCloud("line", new Array(
-		new Point(12,347,1),new Point(119,347,1)
-	));
-	this.PointClouds[9] = new PointCloud("five-point star", new Array(
-		new Point(177,396,1),new Point(223,299,1),new Point(262,396,1),new Point(168,332,1),new Point(278,332,1),new Point(184,397,1)
-	));
-	this.PointClouds[10] = new PointCloud("null", new Array(
-		new Point(382,310,1),new Point(377,308,1),new Point(373,307,1),new Point(366,307,1),new Point(360,310,1),new Point(356,313,1),new Point(353,316,1),new Point(349,321,1),new Point(347,326,1),new Point(344,331,1),new Point(342,337,1),new Point(341,343,1),new Point(341,350,1),new Point(341,358,1),new Point(342,362,1),new Point(344,366,1),new Point(347,370,1),new Point(351,374,1),new Point(356,379,1),new Point(361,382,1),new Point(368,385,1),new Point(374,387,1),new Point(381,387,1),new Point(390,387,1),new Point(397,385,1),new Point(404,382,1),new Point(408,378,1),new Point(412,373,1),new Point(416,367,1),new Point(418,361,1),new Point(419,353,1),new Point(418,346,1),new Point(417,341,1),new Point(416,336,1),new Point(413,331,1),new Point(410,326,1),new Point(404,320,1),new Point(400,317,1),new Point(393,313,1),new Point(392,312,1),
-		new Point(418,309,2),new Point(337,390,2)
-	));
-	this.PointClouds[11] = new PointCloud("arrowhead", new Array(
-		new Point(506,349,1),new Point(574,349,1),
-		new Point(525,306,2),new Point(584,349,2),new Point(525,388,2)
-	));
-	this.PointClouds[12] = new PointCloud("pitchfork", new Array(
-		new Point(38,470,1),new Point(36,476,1),new Point(36,482,1),new Point(37,489,1),new Point(39,496,1),new Point(42,500,1),new Point(46,503,1),new Point(50,507,1),new Point(56,509,1),new Point(63,509,1),new Point(70,508,1),new Point(75,506,1),new Point(79,503,1),new Point(82,499,1),new Point(85,493,1),new Point(87,487,1),new Point(88,480,1),new Point(88,474,1),new Point(87,468,1),
-		new Point(62,464,2),new Point(62,571,2)
-	));
-	this.PointClouds[13] = new PointCloud("six-point star", new Array(
-		new Point(177,554,1),new Point(223,476,1),new Point(268,554,1),new Point(183,554,1),
-		new Point(177,490,2),new Point(223,568,2),new Point(268,490,2),new Point(183,490,2)
-	));
-	this.PointClouds[14] = new PointCloud("asterisk", new Array(
-		new Point(325,499,1),new Point(417,557,1),
-		new Point(417,499,2),new Point(325,557,2),
-		new Point(371,486,3),new Point(371,571,3)
-	));
-	this.PointClouds[15] = new PointCloud("half-note", new Array(
-		new Point(546,465,1),new Point(546,531,1),
-		new Point(540,530,2),new Point(536,529,2),new Point(533,528,2),new Point(529,529,2),new Point(524,530,2),new Point(520,532,2),new Point(515,535,2),new Point(511,539,2),new Point(508,545,2),new Point(506,548,2),new Point(506,554,2),new Point(509,558,2),new Point(512,561,2),new Point(517,564,2),new Point(521,564,2),new Point(527,563,2),new Point(531,560,2),new Point(535,557,2),new Point(538,553,2),new Point(542,548,2),new Point(544,544,2),new Point(546,540,2),new Point(546,536,2)
-	));
+
 	//
 	// The $P Point-Cloud Recognizer API begins here -- 3 methods: Recognize(), AddGesture(), DeleteUserGestures()
 	//
